321c6f56346104c33004eb7088044ed8b1b547e6
Adrian <74800016+acombs801@users.noreply.github.com>
2021-03-08 21:34:44 -0800

    adding server.js, server now runs

---
+++ public/server.js
@@ -0,0 +1,23 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Setting up Express App
+
+const app = express();
+const PORT = 3040;
+
+// 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
