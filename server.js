2a808d9dcd0243a521eafb93ddca89d666c4cc18
Adrian <74800016+acombs801@users.noreply.github.com>
2021-03-08 23:24:58 -0800

    fixing server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 // Setting up Express App
 
 const app = express();
-const PORT = 3040;
+const PORT = 3001;
 
 // 
 
@@ -18,7 +18,5 @@ app.use(express.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-app.get('/api/notes', (req, res) => res.json(note));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
